fd89783ac8cac64e6610b96a2cf8c8261ef432d6
Abdul Ahad <ahadking6152@gmail.com>
2025-08-01 10:17:11 -0700

    Your commit message here

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from Abdul Ahad"; //letter subject
-	$to = 'abdulahad007@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$to = 'abdul.ahadt732@gmail.com'; // Enter Your E-mail
+	$from = 'abdul.ahadt732@gmail.com'; // Admin e-mail
 	$fromName = 'Abdul Ahad'; // Your company name
 	$charset = 'UTF-8';
 ?>
